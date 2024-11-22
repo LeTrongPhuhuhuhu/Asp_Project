1c57fa575740020b748aa20ee7c090979bd65f8a
hoangphuc1116 <hoangphuc16062003@gmail.com>
2024-11-22 16:04:58 +0700

    firstcm

--- 6TL/6TL/obj/Release/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Release/net8.0/6TL.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cf5595fd77d75115a2ceeaa2a49040554a8730d")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
