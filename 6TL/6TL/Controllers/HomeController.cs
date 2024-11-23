08106028027d188922c3cce03250d0a6d5a5e685
hoangthienisme <nguyenhoangthien120304@gmail.com>
2024-11-24 02:14:36 +0700

    Code giao dien cho trang ViewProfile
    tao file gitnore
    
     khac phuc xung dot
    
     thay doi code trong ViewProfile
    
    Push VieProfile up to git
    
    Update 6TL.sln

--- 6TL/6TL/Controllers/HomeController.cs
+++ 6TL/6TL/Controllers/HomeController.cs
@@ -23,7 +23,15 @@ namespace _6TL.Controllers
 			return View();
 		}
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult ViewProfile()
+        {
+            return View();
+        }
+        public IActionResult LichSuMuaHang()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cf5595fd77d75115a2ceeaa2a49040554a8730d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c15785fd084f7b1be288f1d3f9faed4edaef5aac")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
