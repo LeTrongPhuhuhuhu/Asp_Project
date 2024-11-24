1f965d164819acc653f1c1e1547f81b70542ed62
hoangphuc1116 <hoangphuc16062003@gmail.com>
2024-11-24 17:34:40 +0700

    giao diện trang chủ

--- 6TL/6TL/obj/Release/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Release/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cf5595fd77d75115a2ceeaa2a49040554a8730d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aab0af93eba428355fe80b6caf9fba86dcdc2550")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
