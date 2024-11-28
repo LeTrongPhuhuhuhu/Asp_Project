d5922b51a690b6dbba4e52633b8c8ba1d8db34e1
hoangphuc1116 <hoangphuc16062003@gmail.com>
2024-11-28 23:29:44 +0700

    Trang Chu hoan chinh thieu hinh

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc552a044d0d39b8dafaf33ab9229ae0495f7e7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35b3c59dee9b336663f251889fd9c6d874c3e51c")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
