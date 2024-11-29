89c930f10e71077e1961c32e55ba640bb0a95826
hoangphuc1116 <hoangphuc16062003@gmail.com>
2024-11-29 16:38:51 +0700

    trang chu chia css , logo dang nhap, dang ky

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ca0940d947eb9cddd6982cdbaa29af5b6c14b51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+467ffcb45f9fa508cc94f824cebfb175f4032083")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
