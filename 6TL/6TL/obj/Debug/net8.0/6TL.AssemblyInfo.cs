473333eddecbf709c384adeadb4c0f1764e12fed
hoangthienisme <nguyenhoangthien120304@gmail.com>
2024-11-29 16:10:30 +0700

    chinh sua css trang LichSuMuaHang

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ce738fbaa29d4506041d5c108f42e02b5256917")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37f8c94422c54e819bec7217fdd26ecdb7020281")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
