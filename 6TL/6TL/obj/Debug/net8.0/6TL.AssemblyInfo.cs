4ca0940d947eb9cddd6982cdbaa29af5b6c14b51
hoangphuc1116 <hoangphuc16062003@gmail.com>
2024-11-29 15:53:06 +0700

    Trang Chu chia cau truc css

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7737d82b1fed353fae8a6db32261154263baa622")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0bce529b2e54404cd15936f8fa97fd5697fae11")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
