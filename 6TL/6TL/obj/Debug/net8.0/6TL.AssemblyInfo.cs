a378d3d39a59bb5bec613bc6f6b8d45d30ec3eaa
hoangthienisme <nguyenhoangthien120304@gmail.com>
2024-12-01 14:19:20 +0700

    Them ckeditor và popup sua xem

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28de9fa38aee5943328e4c2663d9d0eaadc6e2fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbc861136468eea1d5837ff402558ba7ae37ad8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
