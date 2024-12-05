2bb5802a7581f383a273b5cdac5da145f6791013
hoangthienisme <nguyenhoangthien120304@gmail.com>
2024-12-05 23:30:52 +0700

    update hinh anh va mo ta blog

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22139d512cb5371e2e0507da073eb3a9cef59266")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45b6b91a0aef8e329d22427ad9a91a3a83a9719d")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
