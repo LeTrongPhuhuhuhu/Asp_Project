82bd143fe6ef2a3709c752f782f174292ee4458f
HoanghAnn <hoangaan597@gmail.com>
2024-12-09 10:41:55 +0700

    Push lai nhe

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16ca0f0fdd6c46d050056c1422e34718706516fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0500d85c2bddf55956d39fb405764c7dca3dde")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
