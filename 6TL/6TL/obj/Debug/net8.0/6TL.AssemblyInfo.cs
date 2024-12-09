a8f9b2dabf3f763874315cae5dca7990930f3e1a
hoangthienisme <nguyenhoangthien120304@gmail.com>
2024-12-09 17:02:23 +0700

    update trang gioi thieu ( them blog )

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c7b0d022038816852ba540730f1fb8295e494d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+267585873928eccdd4a9b2aedc4ba6c2b7d7da15")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
