ea8fee273faeb1ce69d4fd1643ab46509ac806da
yagaminguyen <03006221380@caothang.edu.vn>
2024-12-10 23:30:54 +0700

    dashboard updated

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd759fb52364379d2008b0e0b68fbf9fd6536a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4de7dd44db11bd7c48108da9d9a77a232a3ff7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
