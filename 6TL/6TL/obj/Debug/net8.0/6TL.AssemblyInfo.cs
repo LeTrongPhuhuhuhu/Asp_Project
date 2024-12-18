2025c2a9baa3e4f99844e99646b06380010e82ca
hoangthienisme <nguyenhoangthien120304@gmail.com>
2024-12-18 17:21:48 +0700

    chinh css popup sửa danh mục

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da1fc22be707108704551e30e0d54f3deeacfcf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+622f710e6d182b159f660aa3c0262cc89d48babf")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
