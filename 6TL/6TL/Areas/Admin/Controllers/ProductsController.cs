ce879a071914f68b8a801883f7674899ddfec5ab
hoangthienisme <nguyenhoangthien120304@gmail.com>
2024-12-31 16:21:34 +0700

    Thay đổi nhẹ giao diện thêm sản phẩm

---
+++ 6TL/6TL/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using _6TL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace _6TL.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        
+        private Db6TLContext _context;
+
+        public ProductsController(Db6TLContext context) {
+            _context = context;
+        }
+
+        public IActionResult QuanLySanPham()
+        {
+            return View();
+        }
+
+        public IActionResult ThemSanPham(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                
+
+                // Thêm sản phẩm vào cơ sở dữ liệu
+                _context.Products.Add(product);
+                _context.SaveChanges();
+
+                // Chuyển hướng về trang quản lý sản phẩm
+                return RedirectToAction("QuanLySanPham");
+            }
+
+            // Nếu có lỗi, trả lại view thêm sản phẩm
+            return View(product);
+        }
+    
+
+        public IActionResult SuaSanPham()
+        {
+            return View();
+        }
+    }
+}
