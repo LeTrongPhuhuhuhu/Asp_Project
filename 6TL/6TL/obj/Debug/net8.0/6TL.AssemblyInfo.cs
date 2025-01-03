5a85cc1cd82fe501ec25dee6127cc1975b2b99c4
hoangthienisme <nguyenhoangthien120304@gmail.com>
2025-01-03 10:23:14 +0700

    Chinh sua giao dien them san pham

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d4c27d82f1e842537aea389b9b8928533b00215")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9210048ae2a37b3bc71202af2378c3b21087c1d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
