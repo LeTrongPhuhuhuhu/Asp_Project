7ba79048ca7f7b79aa7176fa081f2ba93961a06d
hoangthienisme <nguyenhoangthien120304@gmail.com>
2025-01-03 11:15:23 +0700

    Sua css cho trang them san pham

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9210048ae2a37b3bc71202af2378c3b21087c1d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23798ee49aa742b4cac8b5dd98e29c199ff7b6ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
