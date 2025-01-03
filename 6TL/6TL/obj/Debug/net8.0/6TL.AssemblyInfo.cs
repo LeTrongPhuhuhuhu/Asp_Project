c91341c5d1940351e9530ef2ed144cfaa670166a
LeTrongPhuhuhuhu <lphu7805@gmail.com>
2025-01-03 18:39:12 +0700

    ok

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+622f710e6d182b159f660aa3c0262cc89d48babf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25446d1aa6ebc21abc286bdfe68b98984176e7fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
