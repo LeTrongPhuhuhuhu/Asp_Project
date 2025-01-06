38f5d2c1a8ab66fe8c456d7c8ae012212e143475
hoangthienisme <nguyenhoangthien120304@gmail.com>
2025-01-07 01:28:37 +0700

    Truyen du lieu Category sang chuc nang loc

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc016aad197f3f7c414288869cbc5ec10bbd9498")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8184ad7f6ff96e45c38185ae9d33466557651746")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
