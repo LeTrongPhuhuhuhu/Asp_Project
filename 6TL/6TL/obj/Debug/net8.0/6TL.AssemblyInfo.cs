371b7466d718f31d7a7395b05165b9f594142fde
LeTrongPhuhuhuhu <lphu7805@gmail.com>
2025-01-07 20:24:52 +0700

    cbimerge

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5b6b5446e98a410e654d70c4376d8b69331c115")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bbd04580735ccd5b364e040a770288a2ecf4790")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
