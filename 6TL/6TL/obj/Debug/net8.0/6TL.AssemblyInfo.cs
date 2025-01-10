c5b0a67a7a4d295f58b16eb963b61438ace947c3
viet5464@gmail.com <viet5464@gmail>
2025-01-11 01:24:24 +0700

    push lại hoàn chỉnh qllh

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a0d709acde4decad9318fc849a97f916ab16f0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+010ab545e2c41543fb73433bace54e8983519060")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
