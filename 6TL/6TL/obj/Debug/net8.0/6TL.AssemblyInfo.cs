74cb3fae9eb71613397d667d97301c9531a5ddb1
yagaminguyen <03006221380@caothang.edu.vn>
2025-01-13 14:11:24 +0700

    PUSH TEST

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+365e4840f4a707572efe90671450e3f4c28b3834")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff8d72983cbe1621370425e9f9b42bdf2475a1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
