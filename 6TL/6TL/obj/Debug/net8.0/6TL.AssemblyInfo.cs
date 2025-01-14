ba5dab3c5904d9baee11acdc96ca72f831448b56
yagaminguyen <03006221380@caothang.edu.vn>
2025-01-14 23:40:37 +0700

    da lam xong an hien

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06eee45c694d7eef152e8f1df2e086f906f688a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e63b359c0c5ad6e0005a3b7d95977a9962414517")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
