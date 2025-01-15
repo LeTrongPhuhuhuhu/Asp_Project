90fd2d72fb080c8b84ecc89503861ac088fd2af1
LeTrongPhuhuhuhu <lphu7805@gmail.com>
2025-01-15 17:13:40 +0700

    Đang xử lý thanh toán momo

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58738dc629464958213f6ce2311d1da34c75d1b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2241d6266ba5f3d51d2879f226f2a94c8d82466")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
