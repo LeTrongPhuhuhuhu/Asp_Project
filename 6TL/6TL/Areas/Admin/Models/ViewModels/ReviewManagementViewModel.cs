74af65523c94cc2661a3051a42d8d03762fe2c43
viet5464@gmail.com <viet5464@gmail>
2025-01-17 17:15:19 +0700

    push qldg

---
+++ 6TL/6TL/Areas/Admin/Models/ViewModels/ReviewManagementViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using _6TL.Models;
+
+namespace _6TL.ViewModels
+{
+	public class ReviewManagementViewModel
+	{
+		public IEnumerable<Product> Products { get; set; }
+		public IEnumerable<Review> Reviews { get; set; }
+	}
+}
