e1d476d1cedd57e38a1c080dbc7198fbf357d11a
hoangphuc1116 <hoangphuc16062003@gmail.com>
2025-01-17 16:17:18 +0700

    4:16_1/17/2025 danh sach

--- 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
+++ 6TL/6TL/obj/Debug/net8.0/6TL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6TL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26b26ee91148f1059255ba326c7a3b449bc43c71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a6ff8423c92e7a1815dcfd441ed73dac17fb31")]
 [assembly: System.Reflection.AssemblyProductAttribute("6TL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6TL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
